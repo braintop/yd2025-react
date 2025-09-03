f46808a686c6ae6ba621b67baba53bdd799e7e23
braintop <braintop7@gmail.com>
2025-09-03 11:31:32 +0300

    add2

--- 02-usestate/my-react-app/src/App.jsx
+++ 02-usestate/my-react-app/src/App.jsx
@@ -3,23 +3,32 @@ function App() {
 
   //phase 1 - properties and state
   //let counter = 0;
-  counter [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
   //phase 2 - events and methods
-  function handleClick() {
+  function handleIncrement() {
     //counter++;
     //alert(counter);
-   // setCounter(counter + 1);
+    setCounter(counter + 1);
   }
 
-  //phase 3 - html
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <p>Counter: {counter}</p>
-      <button onClick={handleClick}>Click me</button>
-    </div>
-  )
-}
+  function handleReset() {
+    setCounter(0);
+  }
+  function handleMinus() {
+    setCounter(counter - 1);
+  }
 
-export default App
+    //phase 3 - html
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Counter: {counter}</p>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleMinus}>-</button>
+      </div>
+    )
+  }
+
+  export default App
