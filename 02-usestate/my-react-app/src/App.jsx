5e7294dd18c5d16265ad2fb306f4539349da53a8
braintop <braintop7@gmail.com>
2025-09-03 13:16:11 +0300

    add2

--- 02-usestate/my-react-app/src/App.jsx
+++ 02-usestate/my-react-app/src/App.jsx
@@ -4,19 +4,28 @@ function App() {
   //phase 1 - properties and state
   //let counter = 0;
   const [counter, setCounter] = useState(0);
-
+  const [name, setName] = useState('');
   //phase 2 - events and methods
   function handleIncrement() {
     //counter++;
     //alert(counter);
     setCounter(counter + 1);
   }
-
+  function handleChange(e) {
+    //setName(e.target.value);
+    console.log(e.target.value);
+    setName(e.target.value);
+  }
   function handleReset() {
     setCounter(0);
   }
   function handleMinus() {
     setCounter(counter - 1);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Form submitted");
+    alert(name);
   }
 
     //phase 3 - html
@@ -27,6 +36,14 @@ function App() {
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleReset}>Reset</button>
         <button onClick={handleMinus}>-</button>
+
+        <form onSubmit={handleSubmit}>
+          <input type="text"  onChange={handleChange} placeholder="Enter your name" />
+          <button type="submit">Submit</button>
+
+        </form>
+
+        
       </div>
     )
   }
