ebe1c7a675a793ac00bdad3d119999fa3bca3251
braintop <braintop7@gmail.com>
2025-09-03 10:51:10 +0300

    add1

--- 01-intro/my-react-app/src/App.jsx
+++ 01-intro/my-react-app/src/App.jsx
@@ -1,9 +1,22 @@
 
 function App() {
+  //phase 1 - properties and state
+  let firstName = 'John';
+  let lastName = 'Doe';
+  let age = 20;
+  let isStudent = true;
+  let hobbies = ['reading', 'writing', 'coding'];
 
+  //phase 2 - events and methods
+
+  //phase 3 - html 
   return (
     <>
       <h1>Hello World</h1>
+      <p>Hello {firstName} {lastName}</p>
+      <p>Age: {age}</p>
+      <p>Student: {isStudent ? 'Yes' : 'No'}</p>
+      <p>Hobbies: {hobbies.join(', ')}</p>
     </>
   )
 }
